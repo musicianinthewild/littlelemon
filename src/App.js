41e8a2ae10f3b24a6301c18bf02cdc2ce142bd4d
Gonçalo Pereira <litosantos62@gmail.com>
2023-10-19 19:03:22 +0100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
